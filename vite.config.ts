31e8113e7aab8ba348f88d2b2270621df8df6ef9
Buzaev Roman <rbuzaev1@gmail.com>
2025-02-09 22:44:40 +0300

    feat: project FSD architecture start

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    // ререндер при изменениях
+    react(),
+    // использование svg
+    svgr(),
+
+    // Показывает размер пандлов как вебпаковский analizer
+    visualizer({
+      open: true,
+      filename: 'stats.html',
+      template: 'treemap',
+    }),
+  ],
+  build: {
+    // Минификация
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+});
