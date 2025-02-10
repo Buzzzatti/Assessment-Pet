a0d919f8b992e23b531e3121a8a0d4fb9153895d
Buzaev Roman <rbuzaev1@gmail.com>
2025-02-10 21:21:14 +0300

    feat: 005 fixes

--- scripts/validate-commit-msg.js
+++ scripts/validate-commit-msg.js
@@ -1,13 +1,10 @@
 import fs from 'fs';
 
-// Регулярное выражение для проверки формата сообщения
 const commitMessageRegex = /^(feat|feature|fix|bugfix|release): \d{3} .+$/;
 
-// Читаем сообщение коммита из файла
 const commitMessageFile = process.argv[2];
 const commitMessageRaw = fs.readFileSync(commitMessageFile, 'utf-8');
 
-// Убираем строки, начинающиеся с #
 const commitMessage = commitMessageRaw
   .split('\n')
   .filter(line => !line.startsWith('#'))
@@ -16,7 +13,6 @@ const commitMessage = commitMessageRaw
 
 console.log('Сообщение коммита после фильтрации:', commitMessage);
 
-// Проверяем сообщение на соответствие регулярному выражению
 if (!commitMessageRegex.test(commitMessage)) {
   console.error(
     '\x1b[31m%s\x1b[0m',
