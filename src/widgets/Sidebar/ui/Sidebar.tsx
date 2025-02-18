8d595f58ceb03da7469d0a798268d80b05607978
Buzaev Roman <rbuzaev1@gmail.com>
2025-02-19 01:21:27 +0300

    feat: 011 prettier tabs fix

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -7,17 +7,17 @@ import { List } from '@mui/material';
 import { CustomListItem, CustomListItemText } from './Sidebar.styles';
 
 export const Sidebar = () => {
-  const { pathname } = useLocation();
+    const { pathname } = useLocation();
 
-  return (
-    <List>
-      {menuItems.map(({ path, label }) => (
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        //@ts-expect-error
-        <CustomListItem isActive={pathname === path} key={path} component={NavLink} to={path}>
-          <CustomListItemText primary={label} />
-        </CustomListItem>
-      ))}
-    </List>
-  );
+    return (
+        <List>
+            {menuItems.map(({ path, label }) => (
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                //@ts-expect-error
+                <CustomListItem isActive={pathname === path} key={path} component={NavLink} to={path}>
+                    <CustomListItemText primary={label} />
+                </CustomListItem>
+            ))}
+        </List>
+    );
 };
